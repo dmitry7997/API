ff4a9d9ed8df833c700f63c5b8f2b5c06d482daf
dmitry7997 <dmitry7997@yandex.ru>
2025-09-15 13:09:36 +0300

    created custom cell in UIKit

---
+++ Menu/UIKit/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  Menu
+//
+//  Created by Not Null on 13.09.2025.
+//  Copyright © 2025 DoorDash, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
